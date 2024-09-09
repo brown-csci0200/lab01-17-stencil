5e4fddf9d01ac424df64331e738288f5bcd905f2
Nicholas DeMarinis <ndemarinis@wpi.edu>
2024-09-09 05:22:35 -0400

    Added test example.

--- sol/BSTTest.java
+++ sol/BSTTest.java
@@ -1,4 +1,12 @@
 package sol;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BSTTest {
+
+    @Test
+    public void testExample() {
+        Assert.assertEquals(2, 1 + 1);
+    }
 }
